cee0fa3356ff67f52bc1c2990d4ce4c8dabd5f78
spartanos <eijfm21@gmail.com>
2025-12-29 12:15:16 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'iq-pentagon-v1';
+const CACHE_NAME = 'iq-pentagon-v2';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
 // Install - cache files
